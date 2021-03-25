65f6e832163b8535d6e9a6ebfa03b94d9ba18424
kg4543 <jyc351919@naver.com>
2021-03-25 17:22:55 +0900

    메뉴추가

---
+++ WpfSMSApp/WpfSMSApp/Helper/Commons.cs
@@ -0,0 +1,14 @@
+﻿using NLog;
+using WpfSMSApp.Model;
+
+namespace WpfSMSApp
+{
+    public class Commons
+    {
+        //NLog 정적 인스턴스 생성
+        public static readonly Logger LOGGER = LogManager.GetCurrentClassLogger();
+
+        //Login한 정보 인스턴스 
+        public static User LOGINED_USER; 
+    }
+}
