6fe5d1c5267a2aa988c8e459d45314c1ee582d3e
kg4543 <jyc351919@naver.com>
2021-03-31 12:35:53 +0900

    UserList 및 AddUser 페이지 추가

--- WpfSMSApp/WpfSMSApp/Helper/Commons.cs
+++ WpfSMSApp/WpfSMSApp/Helper/Commons.cs
@@ -1,6 +1,8 @@
 ﻿using NLog;
+using System;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using WpfSMSApp.Model;
 
 namespace WpfSMSApp
@@ -13,9 +15,7 @@ namespace WpfSMSApp
         //Login한 정보 인스턴스 
         public static User LOGINED_USER;
 
-        // public static string SYS_HASH = "pknu_sms";
-
-        // MD5 암호화
+        // MD5 암호화 처리 메소드
         public static string GetMd5Hash(MD5 md5Hash, string plainStr)
         {
             byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(plainStr));
@@ -26,6 +26,17 @@ namespace WpfSMSApp
             }
 
             return builder.ToString();
+        }
+
+        /// <summary>
+        /// 이메일 정규식 확인 메소드
+        /// User.AddUser
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        internal static bool IsValidEmail(string email)
+        {
+            return Regex.IsMatch(email, @"[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?");
         }
     }
 }
--- WpfSMSApp/WpfSMSApp/User/UserList.xaml.cs
+++ WpfSMSApp/WpfSMSApp/User/UserList.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+
+
 namespace WpfSMSApp.View.User
 {
     /// <summary>
@@ -18,25 +22,26 @@ namespace WpfSMSApp.View.User
         {
             try
             {
-                var user = Commons.LOGINED_USER;
-                /*TxtUserID.Text = user.UserID.ToString(); 
-                TxtUserIdentityNumber.Text = user.UserIdentityNumber.ToString(); 
-                TxtUserSurName.Text = user.UserSurname.ToString(); 
-                TxtUserName.Text = user.UserName.ToString(); 
-                TxtUserEmail.Text = user.UserEmail.ToString(); 
-                TxtUserAdmin.Text = user.UserAdmin.ToString(); 
-                TxtUserActivated.Text = user.UserActivated.ToString();*/
+                RdoAll.IsChecked = true;
             }
             catch (Exception ex)
             {
-                Commons.LOGGER.Error($"예외발생 MyAccount Loaded : {ex}");
+                Commons.LOGGER.Error($"예외발생 UserList Loaded : {ex}");
                 throw ex;
             }
         }
 
         private void BtnAddUser_Click(object sender, RoutedEventArgs e)
         {
+            NavigationService.Navigate(new AddUser());
+            try
+            {
 
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 : BtnAddUser_Click : {ex}");
+            }
         }
 
         private void BtnEditUser_Click(object sender, RoutedEventArgs e)
@@ -48,5 +53,67 @@ namespace WpfSMSApp.View.User
         {
 
         }
+
+        private void BtnExportPdf_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RdoAll_Checked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                List<WpfSMSApp.Model.User> users = new List<Model.User>();
+                if (RdoAll.IsChecked == true)
+                {
+                    users = Logic.DataAcess.GetUsers();
+                }
+
+                this.DataContext = users;
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 : {ex}");
+            }
+        }
+
+        private void RdoActive_Checked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                List<WpfSMSApp.Model.User> users = new List<Model.User>();
+
+                if (RdoActive.IsChecked == true)
+                {
+                    users = Logic.DataAcess.GetUsers().Where(u => u.UserActivated == true).ToList();
+                }
+
+                this.DataContext = users;
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 : {ex}");
+            }
+        }
+
+        private void RdoDeactive_Checked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                List<WpfSMSApp.Model.User> users = new List<Model.User>();
+                if (RdoDeactive.IsChecked == true)
+                {
+                    users = Logic.DataAcess.GetUsers().Where(u => u.UserActivated == false).ToList();
+                }
+
+                this.DataContext = users;
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 : {ex}");
+            }
+        }
+
+        
     }
 }
