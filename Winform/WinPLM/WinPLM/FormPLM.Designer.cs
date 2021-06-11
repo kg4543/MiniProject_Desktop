d8034dc08dd97d24ca8d1b1de3d728426b802452
kg4543 <jyc351919@naver.com>
2021-06-11 21:44:37 +0900

    DB 연결 및 데이터 출력

---
+++ Winform/WinPLM/WinPLM/FormPLM.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace WinPLM
+{
+    partial class FormPLM
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DgvData = new System.Windows.Forms.DataGridView();
+            this.BtnItem = new System.Windows.Forms.Button();
+            this.BtnCate = new System.Windows.Forms.Button();
+            this.BtnBrand = new System.Windows.Forms.Button();
+            this.BtnPart = new System.Windows.Forms.Button();
+            this.BtnMaterial = new System.Windows.Forms.Button();
+            this.BtnBom = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DgvData
+            // 
+            this.DgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvData.Location = new System.Drawing.Point(12, 60);
+            this.DgvData.Name = "DgvData";
+            this.DgvData.RowTemplate.Height = 23;
+            this.DgvData.Size = new System.Drawing.Size(505, 215);
+            this.DgvData.TabIndex = 0;
+            this.DgvData.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvData_CellContentClick);
+            // 
+            // BtnItem
+            // 
+            this.BtnItem.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BtnItem.Location = new System.Drawing.Point(13, 13);
+            this.BtnItem.Name = "BtnItem";
+            this.BtnItem.Size = new System.Drawing.Size(79, 30);
+            this.BtnItem.TabIndex = 1;
+            this.BtnItem.Text = "Item";
+            this.BtnItem.UseVisualStyleBackColor = true;
+            this.BtnItem.Click += new System.EventHandler(this.BtnItem_Click);
+            // 
+            // BtnCate
+            // 
+            this.BtnCate.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BtnCate.Location = new System.Drawing.Point(98, 13);
+            this.BtnCate.Name = "BtnCate";
+            this.BtnCate.Size = new System.Drawing.Size(79, 30);
+            this.BtnCate.TabIndex = 2;
+            this.BtnCate.Text = "Categry";
+            this.BtnCate.UseVisualStyleBackColor = true;
+            this.BtnCate.Click += new System.EventHandler(this.BtnCate_Click);
+            // 
+            // BtnBrand
+            // 
+            this.BtnBrand.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BtnBrand.Location = new System.Drawing.Point(183, 13);
+            this.BtnBrand.Name = "BtnBrand";
+            this.BtnBrand.Size = new System.Drawing.Size(79, 30);
+            this.BtnBrand.TabIndex = 3;
+            this.BtnBrand.Text = "Brand";
+            this.BtnBrand.UseVisualStyleBackColor = true;
+            this.BtnBrand.Click += new System.EventHandler(this.BtnBrand_Click);
+            // 
+            // BtnPart
+            // 
+            this.BtnPart.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BtnPart.Location = new System.Drawing.Point(268, 13);
+            this.BtnPart.Name = "BtnPart";
+            this.BtnPart.Size = new System.Drawing.Size(79, 30);
+            this.BtnPart.TabIndex = 4;
+            this.BtnPart.Text = "Part";
+            this.BtnPart.UseVisualStyleBackColor = true;
+            this.BtnPart.Click += new System.EventHandler(this.BtnPart_Click);
+            // 
+            // BtnMaterial
+            // 
+            this.BtnMaterial.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BtnMaterial.Location = new System.Drawing.Point(353, 13);
+            this.BtnMaterial.Name = "BtnMaterial";
+            this.BtnMaterial.Size = new System.Drawing.Size(79, 30);
+            this.BtnMaterial.TabIndex = 5;
+            this.BtnMaterial.Text = "Material";
+            this.BtnMaterial.UseVisualStyleBackColor = true;
+            this.BtnMaterial.Click += new System.EventHandler(this.BtnMaterial_Click);
+            // 
+            // BtnBom
+            // 
+            this.BtnBom.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BtnBom.Location = new System.Drawing.Point(438, 12);
+            this.BtnBom.Name = "BtnBom";
+            this.BtnBom.Size = new System.Drawing.Size(79, 30);
+            this.BtnBom.TabIndex = 6;
+            this.BtnBom.Text = "BOM";
+            this.BtnBom.UseVisualStyleBackColor = true;
+            // 
+            // FormPLM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 440);
+            this.Controls.Add(this.BtnBom);
+            this.Controls.Add(this.BtnMaterial);
+            this.Controls.Add(this.BtnPart);
+            this.Controls.Add(this.BtnBrand);
+            this.Controls.Add(this.BtnCate);
+            this.Controls.Add(this.BtnItem);
+            this.Controls.Add(this.DgvData);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormPLM";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PLM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DgvData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DgvData;
+        private System.Windows.Forms.Button BtnItem;
+        private System.Windows.Forms.Button BtnCate;
+        private System.Windows.Forms.Button BtnBrand;
+        private System.Windows.Forms.Button BtnPart;
+        private System.Windows.Forms.Button BtnMaterial;
+        private System.Windows.Forms.Button BtnBom;
+    }
+}
+
---
+++ Winform/WinPLM/WinPLM/FormPLM.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinPLM
+{
+    public partial class FormPLM : Form
+    {
+        string connString = "Data Source=127.0.0.1;Initial Catalog=ERP;User ID=sa;Password=mssql_p@ssw0rd!;";
+        int i = 0; // 0 = item, 1 = Category, 2 = Brand, 3 = Part, 4 = Material;
+
+        public FormPLM()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshData(i);
+        }
+
+        private void BtnItem_Click(object sender, EventArgs e)
+        {
+            i = 0;
+            RefreshData(i);
+        }
+
+        private void BtnCate_Click(object sender, EventArgs e)
+        {
+            i = 1;
+            RefreshData(i);
+        }
+
+        private void BtnBrand_Click(object sender, EventArgs e)
+        {
+            i = 2;
+            RefreshData(i);
+        }
+
+        private void BtnPart_Click(object sender, EventArgs e)
+        {
+            i = 3;
+            RefreshData(i);
+        }
+
+        private void BtnMaterial_Click(object sender, EventArgs e)
+        {
+            i = 4;
+            RefreshData(i);
+        }
+        private void DgvData_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var selData = DgvData.Rows[e.RowIndex].Cells;
+        }
+
+        // 데이터 로드
+        private void RefreshData(int i)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                string query = "";
+
+                switch (i)
+                {
+                    case 0: // item
+                        query = @"SELECT ItemId
+                                        ,ItemName
+                                        ,ItemDescription
+                                        ,ItemImage
+                                        ,BrandId
+                                        ,CartegoryId
+                                        FROM dbo.Item";
+                        break;
+                    case 1: // category
+                        query = @"SELECT CategoryID
+                                        ,CategoryName
+                                    FROM dbo.ICategory";
+                        break;
+                    case 2: // brand
+                        query = @"SELECT BrandId
+                                        ,BrandName
+                                         FROM dbo.Brand";
+                        break;
+                    case 3: //part
+                        query = @"SELECT PartId
+                                          ,PartName
+                                          ,PartLevel
+                                          ,UpPart
+                                          ,ItemId
+                                          FROM dbo.Part";
+                        break;
+                    case 4: //Material
+                        query = @"SELECT MaterialId
+                                          ,MaterialName
+                                          ,Price
+                                          ,Unit
+                                          ,Quantity
+                                          ,ItemId
+                                          ,PartId
+                                          ,Total_Price = Price * Quantity
+                                      FROM dbo.Material";
+                        break;
+                }
+
+
+                /*SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();*/
+
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+                DgvData.DataSource = ds.Tables[0];
+            }
+        }
+
+        
+    }
+}
